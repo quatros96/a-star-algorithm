2c440e3e8afaf7530544d97fc546fd5ff365f00f
quatros96 <quatros96@gmail.com>
2021-11-14 15:29:59 +0100

    Added comments and better readme.
    ref #3

--- vertex.py
+++ vertex.py
@@ -1,19 +1,24 @@
 import typing as type
 
+# simple Vertex class to easy
+
 
 class Vertex:
     name: int
     position: type.Tuple[int, int]
 
+    # constructor
     def __init__(self, name: int, position: type.Tuple[int, int]) -> None:
         self.name = name
         self.position = position
 
+    # equality function - for comparing instances of this class
     def __eq__(self, o: object) -> bool:
         if not isinstance(o, Vertex):
             return NotImplemented
 
         return self.name == o.name
 
+    # string representation of the instance
     def __repr__(self) -> str:
         return 'Vertex(%s, x: %s, y: %s)' % (self.name, self.position[0], self.position[1])
